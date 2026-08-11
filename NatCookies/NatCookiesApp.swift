43897dd2abda1e4984ea6f3898615dd374eb37d6
JhonHTipas21 <168132558+JhonHTipas21@users.noreply.github.com>
2026-08-11 17:05:57 -0500

    Initial Commit

---
+++ NatCookies/NatCookiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NatCookiesApp.swift
+//  NatCookies
+//
+//  Created by Jhon harvey Tipas solis on 11/08/26.
+//
+
+import SwiftUI
+
+@main
+struct NatCookiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
